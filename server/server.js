09d75640a9de5eb936739d84a7af6121860f619f
rileyale001 <alexjtriley@gmail.com>
2021-09-08 02:53:21 -0700

    cleaning code and making sure it works

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const db = require('./config/connection');
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
-// const routes = require('./routes');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
